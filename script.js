a3041641d5e789c639e6cd31bfc80c924f4e1648
ArinSahu <arinsahu0@gmail.com>
2024-08-10 05:42:18 +0530

    Enhanced

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // console.log("Chala");
 
-let songIndex=0;
+let songIndex=1;
 let audioElement = new  Audio("songs/music1.mp3");
 let masterPlay=document.getElementById("masterPlay");
 let myProgressBar=document.getElementById("myProgressBar");
@@ -9,11 +9,11 @@ let masterSongName=document.getElementById('masterSongName');
 let songItems=Array.from(document.getElementsByClassName('songItem'));
 
 let songs=[
-    {songName:"Sajni Re" , filepath:"Music1.mp3" , coverpath:"covers/cover1.jpg"},
-    {songName:"Dekha tenu" , filepath:"Music2.mp3" , coverpath:"covers/cover2.jpg"},
-    {songName:"Pehle bhi mai" , filepath:"Music3.mp3" , coverpath:"covers/cover3.jpg"},
-    {songName:"Music Name" , filepath:"Music1.mp3" , coverpath:"covers/cover1.jpg"},
-    {songName:"Music Name" , filepath:"Music2.mp3" , coverpath:"covers/cover1.jpg"}
+    {songName:"Sajni Re" , filepath:"Music1.mp3" , coverpath:"covers/cover1.jpeg"},
+    {songName:"Apna bana le piya" , filepath:"Music2.mp3" , coverpath:"covers/cover2.jpeg"},
+    {songName:"Pehle bhi mai" , filepath:"Music3.mp3" , coverpath:"covers/cover3.jpeg"},
+    {songName:"Sanam teri kasam" , filepath:"Music4.mp3" , coverpath:"covers/cover4.jpeg"},
+    {songName:"Tera hone laga hoon" , filepath:"Music5.mp3" , coverpath:"covers/cover5.jpeg"}
 ]
 
 
@@ -24,7 +24,11 @@ songItems.forEach((element,i) => {
 
 masterPlay.addEventListener("click",()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
+        audioElement.play();    
+        let y = document.getElementById(songIndex);
+        console.log(y);
+        y.classList.remove('fa-circle-play');
+        y.classList.add('fa-circle-pause');
         gif.style.opacity=1;
         //document.getElementById('songInfoName').innerHTML=songs[0];
         masterPlay.classList.remove('fa-circle-play');
@@ -33,6 +37,9 @@ masterPlay.addEventListener("click",()=>{
     }
     else{
         audioElement.pause();
+        let eee = document.getElementById(songIndex);
+        eee.classList.remove('fa-circle-play');
+        eee.classList.add('fa-circle-pause');
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
         gif.style.opacity=0;
@@ -60,16 +67,23 @@ const makeAllplays = ()=>{
 
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click',(e)=>{
-        makeAllplays();
-        songIndex=parseInt(e.target.id);
-        e.target.classList.remove('fa-circle-play');
-        e.target.classList.add('fa-circle-pause');
-        audioElement.src=`songs/music${songIndex}.mp3`;
-        audioElement.currentTime=0;
-        audioElement.play();
-        gif.style.opacity=1;
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
+        if(audioElement.paused){
+
+            makeAllplays();
+            songIndex=parseInt(e.target.id);
+            e.target.classList.remove('fa-circle-play');
+            e.target.classList.add('fa-circle-pause');
+            audioElement.src=`songs/music${songIndex}.mp3`;
+            
+            audioElement.play();
+            gif.style.opacity=1;
+            masterPlay.classList.remove('fa-circle-play');
+            masterPlay.classList.add('fa-circle-pause');
+        }
+        else{
+            makeAllplays();
+            audioElement.pause();
+        }
     })
 })
 
@@ -101,10 +115,22 @@ document.getElementById('next').addEventListener('click',()=>{
     makeAllplays();
     audioElement.src=`songs/music${songIndex}.mp3`;
     masterSongName.innerText =songs[songIndex-1].songName;
+    let ee = document.getElementById(songIndex);
+    ee.classList.remove('fa-circle-play');
+    ee.classList.add('fa-circle-pause');
     audioElement.currentTime=0;
     audioElement.play();
     gif.style.opacity=1;
     masterPlay.classList.remove('fa-circle-play');
     masterPlay.classList.add('fa-circle-pause');
 
+})
+document.getElementById('masterPlay').addEventListener('click',()=>{
+   
+    if(audioElement.paused){
+        let x=document.getElementById(songIndex);
+        // console.log(x);
+        x.classList.remove('fa-circle-pause');
+        x.classList.add('fa-circle-play');
+    }
 })
